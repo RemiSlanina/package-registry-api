6479481ee9d8a01b452b714c4ac97b049242deb7
RemiSlanina <remislanina@gmail.com>
2026-06-24 08:57:13 +0200

    Refactor Package to readonly data transfer object DTO using constructor property promotion

--- src/Package.php
+++ src/Package.php
@@ -2,20 +2,12 @@
 
 namespace App;
 
-class Package  {
-    public ?int $id;
-    public string $name;
-    public string $description;
-    public string $programmingLanguage;
-    public string $repositoryUrl;
-    public string $license;
-
-    public function __construct(?int $id, string $name, string $description, string $programmingLanguage, string $repositoryUrl, string $license) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->description = $description;
-        $this->programmingLanguage = $programmingLanguage;
-        $this->repositoryUrl = $repositoryUrl;
-        $this->license = $license;
-    }
+readonly class Package  {
+    public function __construct(
+        public ?int $id,
+        public string $name,
+        public string $description,
+        public string $programmingLanguage,
+        public string $repositoryUrl,
+        public string $license) {}
 }
