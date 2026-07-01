b51764d3d4de9aa8a7e6c42f0b85f51818a6f13b
RemiSlanina <remislanina@gmail.com>
2026-07-01 14:39:38 +0200

    Enable frontend access to API - return JSON content type - add CORS headers for development

--- public/index.php
+++ public/index.php
@@ -2,6 +2,15 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+header("Content-Type: application/json");
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// only for development, remove thereafter
+header("Access-Control-Allow-Origin: *");
+
 use App\PackageController;
 use App\PackageRepository;
 use App\Package;
@@ -18,6 +27,10 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $repository = new PackageRepository($pdo);
 try {
     $controller = new PackageController($repository);
+
+//    var_dump($_SERVER['REQUEST_METHOD']);
+//    var_dump(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+//    die();
 
     $router = new Router($controller);
     $router->handle($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
